c20284ac057753233d624d69835f1456c3d3cc17
Mayank Pandey <mp.pandey86@gmail.com>
2020-07-03 04:02:49 +0530

    Removed old database

--- server.js
+++ server.js
@@ -25,34 +25,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'Mayank',
-      email: 'mp.pandey86@gmail.com',
-      password: 'random',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Naman',
-      email: 'np.naman98@gmail.com',
-      password: 'random1',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'mp.pandey86@gmail.com'
-    }
-  ]
-}
-
 app.get('/', (req, res) => { res.send(db.users) });
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, saltRounds) });
