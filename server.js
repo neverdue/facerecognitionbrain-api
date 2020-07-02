5529395c737c3a8868673fcb2458c74215f1591f
Mayank Pandey <mp.pandey86@gmail.com>
2020-07-03 04:08:34 +0530

    console log PORT

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.put('/image', (req, res) => { image.handleEntryUpdate(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleAPICall(req, res) });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('hello i am working fine');
+  console.log(`app is running on port ${process.env.PORT}`);
 })
