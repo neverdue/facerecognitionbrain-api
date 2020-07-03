dd480227a1c0cbc2c9445c5d48afe901766795d6
Mayank Pandey <mp.pandey86@gmail.com>
2020-07-03 05:52:18 +0530

    Added new database

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : '',
-    password : '',
-    database : 'facerecognition'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
+  }
   }
 });
 
